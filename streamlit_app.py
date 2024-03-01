10e06619ee53d34fc42c6938b246dd9109063d33
parker84 <brydonparker4@gmail.com>
2024-03-01 10:52:35 -0500

    handling files that are too big by splitting them up into 2 and transcribing separately and then combining afterwards

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from openai import OpenAI
+from openai import OpenAI, APIStatusError
 import os
+from pydub import AudioSegment
 
 st.set_page_config(
     page_title="Audio -> Text",
@@ -31,8 +32,39 @@ if audio_file is not None:
             client = OpenAI(
                 api_key=open_api_key
             )
-            transcription = client.audio.transcriptions.create(
-                model="whisper-1", 
-                file=audio_file
-            )
-        st.write(f'**Text**: {transcription.text}')
+            try:
+                transcription = client.audio.transcriptions.create(
+                    model="whisper-1", 
+                    file=audio_file
+                )
+                transcription_text = transcription.text
+            except APIStatusError as e:
+                if e.status_code == 413:
+                    st.warning(f"File is bigger than 26214400 bytes - breaking mp3 into 2 files and trying again...")
+                    with open("audio_file.mp3", "wb") as f:
+                        f.write(audio_file.getbuffer())
+                    sound = AudioSegment.from_mp3("audio_file.mp3")
+                    halfway_point = len(sound) / 2
+                    first_half = sound[:halfway_point]
+                    second_half = sound[halfway_point:]
+                    first_half.export("first_half.mp3", format="mp3")
+                    second_half.export("second_half.mp3", format="mp3")
+
+                    try:
+                        with st.spinner('Transcribing first half...'):
+                            with open("first_half.mp3", "rb") as f:
+                                transcription_first_half = client.audio.transcriptions.create(
+                                    model="whisper-1", 
+                                    file=f
+                                )
+                        with st.spinner('Transcribing second half...'):
+                            with open("second_half.mp3", "rb") as f:
+                                transcription_second_half = client.audio.transcriptions.create(
+                                    model="whisper-1", 
+                                    file=f
+                                )
+                        transcription_text = transcription_first_half.text + transcription_second_half.text
+                    except APIStatusError as e:
+                        st.error(f"⚠️ {e}")
+
+        st.write(f'**Text**: {transcription_text}')
